976ee043ddb58edbc98d6e748bd2ad3171647170
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2024-02-12 18:55:50 +0300

    Add Bootstrap styles

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
-import './assets/main.css'
+import './assets/css/bootstrap.min.css'
+import './assets/js/bootstrap.min.js'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createHead } from 'unhead'
 
 import App from './App.vue'
 import router from './router'
+
+const head = createHead()
+head.push({
+  meta: [
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+    }
+  ]
+})
 
 const app = createApp(App)
 
